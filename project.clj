ae29b8cc09ec4570629cba6126b45ab01c72569d
zualex <Azubarev32@gmail.com>
2021-11-11 14:05:43 +0300

    lein

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject my-sicp "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :main clojure-workshop.core
+  :main sicp.core
 )
