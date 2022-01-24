72a619766312869412a4d8f0df6c3ad191d637f6
zualex <Azubarev32@gmail.com>
2022-01-24 15:52:58 +0300

    Add exercise 1.3

--- src/exercise/1/1.3.clj
+++ src/exercise/1/1.3.clj
@@ -1,4 +1,4 @@
-; Определите процедуру, которая принимает в качестве аргументов три числа и возвращает сумму квадратов двух больших из них.
+(ns exercise.1.1.3)
 
 (defn sum-of-square [a, b]
   (+ (* a a) (* b b)))
@@ -7,20 +7,21 @@
   (cond (and (< n1 n2) (< n1 n3)) (sum-of-square n2 n3)
         (and (< n2 n1) (< n2 n3)) (sum-of-square n1 n3)
         :else (sum-of-square n1 n2)))
-;13
+
 (sum-of-square 2 3)
+; => 13
 
-;13
 (sum-of-square-only-two-large 2 3 1)
+; => 13
 
-;2
 (sum-of-square-only-two-large 1 1 1)
+; => 2
 
-;8
 (sum-of-square-only-two-large 1 2 2)
+; => 8
 
-;5
 (sum-of-square-only-two-large 1 1 2)
+; => 5
 
-;25
 (sum-of-square-only-two-large 1 4 3)
+; => 25
